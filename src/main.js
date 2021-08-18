f77232f52a0e080878d2bbcb6101c0471b5a0120
Kelsey Jackson <kelseythej@gmail.com>
2021-08-18 11:39:59 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import store from "./store";
+import AuthHandler from "./components/AuthHandler";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  mode: "history",
+  routes: [{ path: "/oauth2/callback", component: AuthHandler }],
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
