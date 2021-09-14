dab076fce8c5cc4ee24a9ba7f618792097f4f14d
Kelsey Jackson <kelseythej@gmail.com>
2021-09-13 23:13:57 -0500

    module implementation

---
+++ src/store/modules/images.js
@@ -0,0 +1,18 @@
+const state = {
+    images: []
+}
+
+const getters = {
+    allImages: state => state.images
+}
+const actions = {
+    fetchImages() {
+        
+    }
+}
+
+const mutations = {
+    setImages: (state, images) => {
+        state.images = images
+    }
+}
