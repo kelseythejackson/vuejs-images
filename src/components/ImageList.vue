f8c1e96782e64fe38d22ee4597165f6041882028
Kelsey Jackson <kelseythej@gmail.com>
2021-09-27 00:10:29 -0500

    finished course

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
+    <div v-if="isLoggedIn" class="image-container">
     <img v-for="image in allImages" :key="image.id" :src="image.link" alt="image.description">
   </div>
+  <h2 v-else>Login to get started</h2>
+  </div>
+  
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'ImageList',
-  computed: mapGetters(['allImages']),
+  computed: mapGetters(['allImages', 'isLoggedIn']),
   methods: mapActions(['fetchImages']),
   created() {
     this.fetchImages()
@@ -16,4 +20,16 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .image-container {
+    column-count: 3;
+    column-gap: 0;
+  }
+
+  img {
+    max-width: 100%;
+    padding: 5px;
+  }
+
+
+</style>
--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur'
+import {router } from '../../main'
 const state = {
     images: []
 }
@@ -17,8 +18,9 @@ const actions = {
         // Get the access token
         const { token } = rootState.auth;
         // Cakk iyr api module to do the upload
-        await api.upload(images, token)
+        await api.uploadImages(images, token)
         // Redirect our user to ImageList component
+        router.push('/')
     }
 }
 
